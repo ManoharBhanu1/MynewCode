e75850253d0ab83f96c5715416660bdad05b47f7
ManoharBhanu1 <87259624+ManoharBhanu1@users.noreply.github.com>
2025-06-25 17:12:01 +0530

    deleted caluclation part

--- app.java
+++ app.java
@@ -1,8 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int a=10, b=20;
-        System.out.println(a+b);
-        System.out.println(a-b);
+       
     }
 }
