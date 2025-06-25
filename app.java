82a6477f838caa788e68c2c3a5a51e772d91436f
ManoharBhanu1 <87259624+ManoharBhanu1@users.noreply.github.com>
2025-06-25 17:02:29 +0530

    Update app.java added caluclation data

--- app.java
+++ app.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int a=10, b=20;
+        System.out.println(a+b);
     }
 }
